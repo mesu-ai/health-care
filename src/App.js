6ee037347bc03acb7d033b38fe9bc47444bdf05d
mesu-ai <mesu.s.ss@gmail.com>
2021-10-18 17:26:15 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import NotFind from './components/notfind/NotFind';
 import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Services from './components/services/Services';
+import About from './components/about/About';
+import Login from './components/login/Login';
 
 
 function App() {
@@ -12,10 +16,23 @@ function App() {
     <div className="App">
 
       <BrowserRouter>
+      <Header></Header>
       <Switch>
         
         <Route path='/home'>
           <Home></Home>
+        </Route>
+
+        <Route path='/services'>
+        <Services></Services>
+        </Route>
+
+        <Route path='/aboutus'>
+          <About></About>
+        </Route>
+        
+        <Route path='/login'>
+          <Login></Login>
         </Route>
 
         <Route exact path='/'>
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,9 +1,52 @@
 import React from 'react';
+import './Header.css';
+import { Container, Nav, Navbar} from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
+import brand from '../../image/heartlogo.png';
 
 const Header = () => {
     return (
         <div>
-            
+
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
+        <Container>
+
+        {/* <Navbar.Brand className="brand-name" to='/home'>Heart-Care</Navbar.Brand> */}
+        <Navbar.Brand className="d-flex align-items-center">
+            <img
+                src={brand}
+                width="50"
+                height="50"
+                className="d-inline-block align-top"
+                alt="Heart Care"
+            />{' '}
+           <span className="brand-name">Heart <span className="text-danger">Care</span> </span> 
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+           
+            <Nav className="me-auto d-flex align-items-center">
+
+            <NavLink activeClassName="active" className="navlink" to='/home'>Home</NavLink>
+            <NavLink activeClassName="active" className="navlink" to='/services'>Service</NavLink>
+            <NavLink activeClassName="active" className="navlink" to='/aboutus'>About Us</NavLink>
+
+            {/* <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                <NavDropdown.Item to="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item to="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item to="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item to="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown> */}
+            </Nav>
+            <Nav className="d-flex align-items-center">
+            <NavLink className="navlink" to='login'>Login</NavLink>
+            </Nav>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
+                    
         </div>
     );
 };
