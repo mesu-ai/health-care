9ee7f6b6541677a0f1bee45f68331cc7fbca32d9
mesu-ai <mesu.s.ss@gmail.com>
2021-10-18 13:16:28 +0600

    initialize health care

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD7wpqeLIQMYjUF2vDjNLP1-ciePUUp5DU",
+    authDomain: "heart-care-bd.firebaseapp.com",
+    projectId: "heart-care-bd",
+    storageBucket: "heart-care-bd.appspot.com",
+    messagingSenderId: "675316147005",
+    appId: "1:675316147005:web:5db7cf51d97cef2cfb5b54",
+    measurementId: "G-L017L1N4TQ"
+  };
+
+  export default firebaseConfig;
